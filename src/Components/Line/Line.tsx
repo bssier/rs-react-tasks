4045472f677c2244286f58a94cf3e3792a62a18d
Michael Tavyryn <mihailtavyrin2@gmail.com>
2026-05-29 20:13:04 +0300

    fix: change regexp for -, add semantic tags in line

--- src/Components/Line/Line.tsx
+++ src/Components/Line/Line.tsx
@@ -1,6 +1,6 @@
 import './line.css';
 
-interface Item {
+export interface Item {
   hp: number;
   attack: number;
   defense: number;
@@ -11,18 +11,33 @@ interface Item {
 
 interface Props {
   item: Item;
+  isChecked: boolean;
+  handleCheckboxChange: () => void;
 }
 
-export const Line = ({ item }: Props) => {
+export const Line = ({ item, isChecked, handleCheckboxChange }: Props) => {
   const { hp, attack, defense, speed, img, title } = item;
   return (
     <article className={'line'}>
-      <div className={'title'}>
+      <div className={'checkbox-container'}>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          id={`checkbox-${title}`}
+          onChange={handleCheckboxChange}
+          onClick={(e: React.MouseEvent<HTMLInputElement>) =>
+            e.stopPropagation()
+          }
+          className="card-checkbox"
+          aria-label={`select ${title}`}
+        />
+      </div>
+      <h3 className={'title'}>
         {title === ''
           ? 'Unknown Pokemon'
           : title[0].toUpperCase() + title.slice(1)}
         <img className={'image'} src={img} alt={title}></img>
-      </div>
+      </h3>
       <div className={'stats-wrapper'}>
         <div className={'stats'}>Speed: {speed}</div>
         <div className={'stats'}>Defense: {defense}</div>
