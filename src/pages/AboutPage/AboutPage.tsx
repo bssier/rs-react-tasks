8b3cc71f4464bdbcbfff2abdf495f8d0d4e53fc3
Michael Tavyryn <mihailtavyrin2@gmail.com>
2026-05-29 17:53:52 +0300

    fix: grammar mistakes in text

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,12 +1,15 @@
 import './about-page.css';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
+import { useTheme } from '../../context.ts';
 
 export const AboutPage = () => {
+  const { theme } = useTheme();
+
   return (
-    <article className={'about-page'}>
+    <article className={`about-page ${theme === 'dark' ? 'dark-mode' : ''}`}>
       <div className={'greeting-menu'}>
         <p>
-          Hi, dear user! As you might have guessed, this is a app about search
+          Hi, dear user! As you might have guessed, this is an app for searching
           pokemons. If you want to find something, enter the pokemon name but it
           must be strictly in English.
         </p>
