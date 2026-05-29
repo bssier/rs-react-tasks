9c5644ee1479af002d2ae08b041ef15587c2b449
Michael Tavyryn <mihailtavyrin2@gmail.com>
2026-05-29 15:07:42 +0300

    fix: move MainLayout outside App

---
+++ src/Components/MainLayout/MainLayout.tsx
@@ -0,0 +1,20 @@
+import {Header} from "../Header/Header.tsx";
+import {Outlet} from "react-router-dom";
+import {Flyout} from "../Flyout/Flyout.tsx";
+import {Footer} from "../Footer/Footer.tsx";
+
+interface MainLayoutProps  {
+    handleSearch: (query: string) => void,
+    searchQuery: string
+}
+
+export const MainLayout = ({handleSearch, searchQuery}: MainLayoutProps) => {
+    return (
+        <div className={'app-layout'}>
+            <Header handleSearch={handleSearch} searchQuery={searchQuery}/>
+            <Outlet/>
+            <Flyout/>
+            <Footer/>
+        </div>
+    );
+};
