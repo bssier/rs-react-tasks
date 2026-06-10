10c2ffc890210b9338ac8634d20628472da52cf1
bssier <mihailtavyrin@gmail.com>
2026-06-10 17:31:17 +0300

    fix: rename folders name

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './App.css';
 import { HomePage } from './pages/home-page/HomePage';
 import { NotFoundPage } from './pages/not-found/NotFoundPage';
