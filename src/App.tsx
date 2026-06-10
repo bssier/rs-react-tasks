d825178d08dc17c9a23b75d67a2ac9c171336b2e
bssier <mihailtavyrin@gmail.com>
2026-06-10 23:51:27 +0300

    feat: add config

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,25 @@ import { ErrorBoudary } from './components/error-boudary/ErrorBoudary.tsx';
 import { AboutPage } from './pages/about-page/AboutPage.tsx';
 import { ElementDetail } from './components/pokemon-detail/ElementDetail.tsx';
 import { MainLayout } from './components/main-layout/MainLayout.tsx';
+import { AppProviders } from './components/app-providers/AppProviders';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <ErrorBoudary>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path={'/'} element={<HomePage />}>
-              <Route path="pokemon/:name" element={<ElementDetail />} />
+    <ErrorBoudary>
+      <BrowserRouter>
+        <AppProviders>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path={'/'} element={<HomePage />}>
+                <Route path="pokemon/:name" element={<ElementDetail />} />
+              </Route>
+              <Route path={'/about'} element={<AboutPage />}></Route>
             </Route>
-            <Route path={'/about'} element={<AboutPage />}></Route>
-          </Route>
 
-          <Route path={'*'} element={<NotFoundPage />} />
-        </Routes>
-      </ErrorBoudary>
-    </BrowserRouter>
+            <Route path={'*'} element={<NotFoundPage />} />
+          </Routes>
+        </AppProviders>
+      </BrowserRouter>
+    </ErrorBoudary>
   );
 };
