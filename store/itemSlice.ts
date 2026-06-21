8f0e46c4dafbcc9182809117d11c90308104d1d9
bssier <mihailtavyrin@gmail.com>
2026-06-21 15:33:32 +0300

    fix: error with types

--- store/itemSlice.ts
+++ store/itemSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
-import type { Item } from '../../bssier-REACT2026Q2/src/components/line/Line';
+import type { Item } from '../components/line/Line';
 
 export interface ItemState {
   selectedItems: Item[];
