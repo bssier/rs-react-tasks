3de31d6b9c0c28f7300123503d241ab333495903
bssier <mihailtavyrin@gmail.com>
2026-06-24 11:51:51 +0300

    fix: code duplicate

--- src/components/pokemon-detail/ElementDetail.tsx
+++ src/components/pokemon-detail/ElementDetail.tsx
@@ -21,21 +21,6 @@ export const ElementDetail = () => {
     void navigate(`/${location.search}`);
   };
 
-  if (!item) {
-    return (
-      <article className="element-detail">
-        <div className="close">
-          <button type="button" onClick={handleCloseClick}>
-            <img src={closeIcon} alt="close" />
-          </button>
-        </div>
-        <div className="info">
-          <p>Data not found. Please go back to the list.</p>
-        </div>
-      </article>
-    );
-  }
-
   return (
     <article className="element-detail">
       <div className="close">
@@ -43,22 +28,27 @@ export const ElementDetail = () => {
           <img src={closeIcon} alt="close" />
         </button>
       </div>
+
       <div className="info">
-        <div>
-          <h1>{name}</h1>
-          <section className="detailed-info">
-            <p>Height: {item.height}</p>
-            <p>Weight: {item.weight}</p>
-            <h3>Abilities:</h3>
-            <ul>
-              {item.abilities.map((abilityName: string) => (
-                <li key={abilityName} className="abilities">
-                  {abilityName}
-                </li>
-              ))}
-            </ul>
-          </section>
-        </div>
+        {item ? (
+          <div>
+            <h1>{name}</h1>
+            <section className="detailed-info">
+              <p>Height: {item.height}</p>
+              <p>Weight: {item.weight}</p>
+              <h3>Abilities:</h3>
+              <ul>
+                {item.abilities.map((abilityName: string) => (
+                  <li key={abilityName} className="abilities">
+                    {abilityName}
+                  </li>
+                ))}
+              </ul>
+            </section>
+          </div>
+        ) : (
+          <p>Data not found. Please go back to the list.</p>
+        )}
       </div>
     </article>
   );
