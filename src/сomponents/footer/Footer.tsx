86d685ba96827e5981deff4c1eed98191c46f258
bssier <mihailtavyrin@gmail.com>
2026-07-17 03:18:04 +0300

    feat: add merge api queries

--- src/сomponents/footer/Footer.tsx
+++ src/сomponents/footer/Footer.tsx
@@ -7,9 +7,13 @@ export const Footer = () => {
   const t = useTranslations('Footer');
 
   return (
-    <footer>
-      <nav className={'links-container'}>
-        <a href={'https://github.com/bssier'} target="_blank" rel="noreferrer">
+    <footer className="footer">
+      <nav className="links-container">
+        <a
+          href={'https://github.com/bssier'}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
           {t('gh')}
         </a>
         <a
@@ -17,11 +21,15 @@ export const Footer = () => {
             'https://github.com/rolling-scopes-school/bssier-REACT2026Q2/tree/class-components'
           }
           target="_blank"
-          rel="noreferrer"
+          rel="noreferrer noopener"
         >
           {t('proj')}
         </a>
-        <a href={'https://rs.school/'} target="_blank" rel="noreferrer">
+        <a
+          href={'https://rs.school/'}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
           {t('course')}
         </a>
         <a
@@ -29,7 +37,7 @@ export const Footer = () => {
             'https://www.linkedin.com/in/michael-tavyrin-9b84833bb/?skipRedirect=true'
           }
           target="_blank"
-          rel="noreferrer"
+          rel="noreferrer noopener"
         >
           {t('dev-link')}
         </a>
